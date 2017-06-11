da908830fa5e986a6b28678d52a9b5a41ffd43ea
PerseusH <584148102@qq.com>
2017-06-12 00:17:03 +0800

    first upload

---
+++ view.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def hello(request):
+    # return HttpResponse("Hello Django ! ")
+    return render(request, 'hello.html', {'name': 'Perseus of Github'})
