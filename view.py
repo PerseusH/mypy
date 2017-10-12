77c3161dd2ae3b5d6033258f13ea8acc3a0ab391
PerseusH <584148102@qq.com>
2017-10-12 20:00:22 +0800

    refresh

--- view.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def hello(request):
-    # return HttpResponse("Hello Django ! ")
-    return render(request, 'hello.html', {'name': 'Perseus of Github'})
